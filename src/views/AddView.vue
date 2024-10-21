73f39bc5acbd7c324d38e8cadda7f554e53cbc89
Andy <andyho66@gmail.com>
2024-10-21 02:53:28 -0700

    Developed the add task feature and used list rendering to display each task

---
+++ src/views/AddView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, provide } from 'vue'
+
+let id = 0
+
+const newTask = ref('')
+const todos = ref([])
+
+const emit = defineEmits(['response'])
+
+
+
+const addToDo = () => {
+  emit('response', newTask.value)
+  newTask.value = ''
+
+}
+
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>Add Tasks to To Do List</h1>
+    <form id="add" @submit.prevent="addToDo">
+        <input v-model="newTask" required placeholder="Enter new task">
+        <button>Add Task</button>
+    </form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  #add{
+    margin: 10 10 10 10;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+
+const props = defineProps ({
+  tasks: Array
+})
+
+</script>
+
+<template>
+  <main>
+    <ul v-if="tasks.length > 0">
+      <li v-for="todo in props.tasks" :key = "todo.id">
+        <input type="checkbox" v-model="todo.done">
+        <span>{{ todo.text }}</span>
+        <span>{{ todo.date }}</span>
+      </li>
+    </ul>
+    <h1 v-else>
+      No Tasks Remaining
+    </h1>
+    <TheWelcome />
+  </main>
+</template>
